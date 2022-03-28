f8fda2af8f672119627c14b30d3669fc546ed851
Muhammad Mufid Ridho <muhammadmufidridho77@gmail.com>
2022-03-28 11:57:08 +0700

    kerangka kondisi menggunakan if else

---
+++ walkingBoard.js
@@ -0,0 +1,15 @@
+// pr dulu : gimana cara buat tanda panah di vs ?
+
+
+// merancang kondisi secara manual dulu tanpa fungsi
+if (n == '->') {
+    } 
+    
+else if(n == '<-') {
+    }
+
+else if(n == ''){
+
+}
+
+else if(n == ''){}
