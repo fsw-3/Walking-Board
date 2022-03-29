500d17dc33d20c936cc7d6f7034a9323d12d1ba6
fajarbagass <fajarbagassetiawan17@gmail.com>
2022-03-29 22:22:32 +0700

    looping array board

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("hello bagas");
+function gameControl(board, control){
+    // melakukan looping array board
+    for(let i=0; i<board.length; i++){
+        for(let j=0; j<board.length; j++){
+            // menentukan posisi array board bernilai *
+            if(board[i][j] == '*'){
+                console.log(board[i][j]);
+                console.log(`posisi array bernilai * ada di baris ke ${i} kolom ke ${j}`);
+            }
+        }
+    }
+}
+
+let board1 = [
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '*', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', '']
+];
+  
+console.log(gameControl(board1, ['↑', '←', '↑', '→']));
