b269ec240c009d9f7f8821a23acd9594c3457c2d
arfcrrr <ajengarifa.char@gmail.com>
2022-03-30 11:26:42 +0700

    [FIX] looping logic on gameControl function

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
 const gameControl = (gameBoard, direction) => {
-    for (let index = 0; index < direction.length; index++) {
+    index = 0;
+    while (index < direction.length) {
         for (let row = 0; row < gameBoard.length; row++) {
             for (let col = 0; col < gameBoard.length; col++) {
                 if (gameBoard[row][col] === '*') {
-                    gameBoard[row][col] = '';
                     switch (direction[index]) {
                         case '↑':
+                            gameBoard[row][col] = '';
                             gameBoard[row - 1][col] = '*';
                             break;
                         case '←':
+                            gameBoard[row][col] = '';
                             gameBoard[row][col - 1] = '*';
                             break;
                         case '↓':
+                            gameBoard[row][col] = '';
                             gameBoard[row + 1][col] = '*';
                             break;
                         case '→':
+                            gameBoard[row][col] = '';
                             gameBoard[row][col + 1] = '*';
                             break;
                         default:
-                            console.log('Unknown input!');
+                            gameBoard[row][col] = '*';
                     }
+                    index++;
                 }
             }
         }
     }
     return gameBoard;
 }
+
+// driver code
 
 let board1 = [
     ['', '', '', '', ''],
@@ -34,5 +41,13 @@ let board1 = [
     ['', '', '', '', ''],
     ['', '', '', '', '']
 ]
+let board2 = [
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '*', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', '']
+]
 
-console.log(gameControl(board1, ['↑', '←', '↑']))
+console.log(gameControl(board1, ['↑', '←', '↑', '→']))
+console.log(gameControl(board2, ['↓', '←', '↑', '→', '→', '↑', '→']))
