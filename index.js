3a53b0a5d33986d89954e31ae45219aa3e2da293
MalekFazar <fazarmalek@gmail.com>
2022-03-30 13:49:37 +0700

    Logic buat mendeteksi * Added

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-console.log("hello world");
-// coba coba
-// ini diubah
+let board1 = [
+    [' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', '*', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ']
+  ]
+
+function gameControl(board, movement){
+    let result = [];
+
+    for(i = 0; i < movement.length; i++){
+        let pesan = "";
+        let row = 0;
+        let column = 0;
+        for(j = 0; j < board.length; j++){
+            result[j] = [];
+            for(k = 0; k < board[j].length; k++){
+                switch(board[j][k]){
+                    case "*" :
+                        row = j;
+                        column = k;
+                }
+            }
+        }
+        console.log("ini baris : " + (row + 1));
+        console.log("ini kolom : " + (column + 1));
+        //return pesan;
+    }
+}
+
+console.log(gameControl(board1, ['↑', '←']))
+
+// ['↑', '←', '↑', '→']
