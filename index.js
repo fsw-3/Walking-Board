f81fb5cdc0e70cf3ca6753e63377d6bb7aec66a1
JerrelAdriel <77767187+JerrelAdriel@users.noreply.github.com>
2022-03-30 15:06:16 +0700

    finish attempt

--- index.js
+++ index.js
@@ -1,3 +1,66 @@
-console.log("hello world");
-// coba coba
-// ini diubah
+function gameControl(board,array) {
+    console.log(board);
+    console.log(array);
+    let result = [];
+    let x;
+    let y;
+
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board.length; j++) {
+            if (board[i][j] == '*') {
+                x = i;
+                y = j;
+            }
+        }            
+    }
+    
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] == '↑') {
+            x -= 1;
+        }
+        else if(array[i] == '←'){
+            y -= 1;
+        }
+        else if(array[i] == '↓'){
+            x += 1;;
+        }
+        else{
+            y += 1;
+        }
+    }
+
+    for (let i = 0; i < board.length; i++) {
+        let tes = [];
+        for (let j = 0; j < board.length; j++) {
+            if (i == x && j == y) {
+                tes.push('*')
+            }
+            else{
+                tes.push('');
+            }         
+        }
+        result.push(tes);            
+    }
+
+    return result
+}
+
+let board1 = [
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '*', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', '']
+  ]
+  
+
+console.log(gameControl(board1, ['↑', '←', '↑', '→']));
+
+let board2 = [
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '*', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', '']
+  ]
+  console.log(gameControl(board2, ['↓', '←', '↑', '→', '→', '↑', '→']))
