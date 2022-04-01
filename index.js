46226750df7cb5079ea76926422e1ec47eb283d8
Sulaiman <sulaiman.saleh@widyatama.ac.id>
2022-04-01 17:20:23 +0700

    revisi board-1

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const walkingBoard = (board, arah) => {
-    let k = 0;
-    while (k < arah.length) {
-      for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < board[i].length; j++) {
-            if (board[i][j] === arah[k]) {
-                board[i][j] = 'x';
+    let i = 0;
+    while (i < arah.length) {
+      for (let j = 0; j < board.length; j++) {
+        for (let k = 0; k < board[j].length; k++) {
+          if (board[j][k] === arah[i]) {
+              board[j][k] = 'x';
+          }
+          if (board[j][k] == "*") {
+            if (arah[i] == "↑") {
+              if (j - 1 >= 0) {
+                board[j - 1][k] = "*";
+                board[j][k] = "";
+              }
+            } else if (arah[i] == "←") {
+                if (k - 1 >= 0) {
+                  board[j][k - 1] = "*";
+                  board[j][k] = "";
+                }
+            } else if (arah[i] == "↓") {
+                if (i + 1 < board.length) {
+                  board[j + 1][k] = "*";
+                  board[j][k] = "";
+                }
+            } else if (arah[i] == "→") {
+                if (k + 1 < board[j].length) {
+                  board[j][k + 1] = "*";
+                  board[j][k] = "";
+                }
             }
+            i++;
+          }
         }
       }
     }
-  
     return board;
   };
   
