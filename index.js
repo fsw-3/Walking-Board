6162e78ba7383d42fdffaaa7372a940f9c51a484
daengearly <ewako.mks13@gmail.com>
2022-04-01 20:09:54 +0700

    add function gameControl to index.js and connect index.js to index.html

--- index.js
+++ index.js
@@ -1,3 +1,67 @@
-console.log("hello world");
-// coba coba
-// ini diubah
+function gameControl(board, move) {
+  for (let a = 0; a < board.length; a++) {
+    for (let b = 0; b < board[a].length; b++) {
+      for (let c = 0; c < move.length; c++) {
+        if (board[a][b] === "*") {
+          if (move[c] == "↑") {
+            if (a - 1 >= 0) {
+              board[a][b] = "";
+              board[a - 1][b] = "*";
+            }
+          } else if (move[c] == "←") {
+            if (b - 1 >= 0) {
+              board[a][b] = "";
+              board[a][b - 1] = "*";
+            }
+          } else if (move[c] == "↓") {
+            if (a + 1 < board.length) {
+              board[a][b] = "";
+              board[a + 1][b] = "*";
+            }
+          } else if (move[c] == "→") {
+            if (b + 1 < board[a].length) {
+              board[a][b] = "";
+              board[a][b + 1] = "*";
+            }
+          }
+        }
+      }
+    }
+  }
+  return board;
+}
+
+// Contoh 1:
+let board1 = [
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "*", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+];
+console.log(gameControl(board1, ["↑", "←", "↑", "→"]));
+// output
+// [
+//   ['', '', '*', '', ''],
+//   ['', '', '', '', ''],
+//   ['', '', '', '', ''],
+//   ['', '', '', '', ''],
+//   ['', '', '', '', '']
+// ]
+// Contoh 2:
+let board2 = [
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "*", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+];
+console.log(gameControl(board2, ["↓", "←", "↑", "→", "→", "↑", "→"]));
+//output
+// [
+//   ['', '', '', '', ''],
+//   ['', '', '', '', '*'],
+//   ['', '', '', '', ''],
+//   ['', '', '', '', ''],
+//   ['', '', '', '', '']
+// ]
